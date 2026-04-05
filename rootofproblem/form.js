5dd1afff5f3ee151f47ff7bebf24e470f00e2529
HaileyStahmann <sta21055@byui.edu>
2026-04-05 00:58:18 -0600

    Updated page

--- rootofproblem/form.js
+++ rootofproblem/form.js
@@ -2,6 +2,7 @@ const textarea = document.getElementById('description');
 const charCount = document.getElementById('char-count');
 const feedbackElement = document.getElementById('feedback');
 const formElement = document.querySelector('form'); // FIXED
+const nameArea = document.getElementById('name');
 
 const maxLength = 200;
 
@@ -16,9 +17,18 @@ formElement.addEventListener('submit', function (e) {
     e.preventDefault();
 
     const currentLength = textarea.value.length;
+    const nameLength = nameArea.value.length;
 
     if (currentLength > maxLength) {
         feedbackElement.innerHTML = "Failed to submit: Description is too long.";
+        feedbackElement.style.display = "block";
+        return;
+    } else if (currentLength == 0) {
+        feedbackElement.innerHTML = "Failed to submit: Description is empty.";
+        feedbackElement.style.display = "block";
+        return;
+    } else if (nameLength == 0) {
+        feedbackElement.innerHTML = "Failed to submit: Name is empty.";
         feedbackElement.style.display = "block";
         return;
     }
